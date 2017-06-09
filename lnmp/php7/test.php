eb6e2cabdd4c34382b2972a94c35944c6f51672f
liaolingjia <liaolingjia@163.com>
2017-06-09 17:05:19 +0800

    first

--- lnmp/php7/test.php
+++ lnmp/php7/test.php
@@ -6,12 +6,12 @@ ini_set("display_errors", 1);
 
 //mysql
 try {
-    $conn = new PDO('mysql:host=mysql;port=3306;dbname=singshine;charset=utf8', 'root', 'root');
+    $conn = new PDO('mysql:host=mysql;port=3306;dbname=你的数据库名称;charset=utf8', 'root', 'root');
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
 //$conn->exec('set names utf8');
-$sql = "SELECT * FROM `tiyu_active` WHERE 1";
+$sql = "SELECT * FROM `你的表名` WHERE 1";
 $result = $conn->query($sql);
 while($rows = $result->fetch(PDO::FETCH_ASSOC)) {
     echo $rows['id'] . ' ' . $rows['title']."<br />\n";
